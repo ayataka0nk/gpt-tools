2e8423266263df62fd62ce674d814600ccdb0b6b
Naoto Kamba <naoto.kamba@outlook.jp>
2023-06-19 20:48:19 +0900

    会話機能を使うにはユーザログインを必要とする

--- app/chat/router.py
+++ app/chat/router.py
@@ -3,6 +3,8 @@ from fastapi import APIRouter, Depends
 from fastapi.responses import StreamingResponse
 from .services import create_chat_completion
 from config import Settings, get_settings
+from app.users import User
+from app.auths import get_user
 
 router = APIRouter(
     prefix='/chat',
@@ -11,6 +13,6 @@ router = APIRouter(
 
 
 @router.post('/request-reply')
-def request_reply(settings: Annotated[Settings, Depends(get_settings)]):
+def request_reply(user: Annotated[User, Depends(get_user)], settings: Annotated[Settings, Depends(get_settings)]):
     result = create_chat_completion(api_key=settings.openai_api_key)
     return StreamingResponse(result, media_type="text/event-stream")
